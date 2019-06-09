8bca06a67b1faa4cf321c852783ebb840a96ca25
Daniel Páez <danielpaezsw66@gmail.com>
2019-06-09 18:00:07 -0400

    Practing the React Component's lifecycle

--- src/components/pages/Badges.js
+++ src/components/pages/Badges.js
@@ -4,6 +4,8 @@ import NavBar from "../NavBar.js"
 import Layout from "../Layout.js"
 
 import confLogo from "../../images/badge-header.svg"
+import loading from "../../images/loading.gif"
+
 import BadgesList from "../BadgesList.js"
 
 import { Switch, Link} from "react-router-dom";
@@ -13,8 +15,31 @@ import "../styles/Badges.css"
 
  class Badges extends Component {
 
- 	state = {
- 		data: [
+
+constructor(props) {
+	super(props);
+	console.log(1, "Constructor");
+
+
+ 	this.state = {
+
+ 		data: [],
+ 		loading: true
+ 	}
+
+}
+
+componentDidMount() {
+
+	console.log(3, "Component Did Mount")
+
+	this.timeoutId = setTimeout( 
+	 () => {
+
+	 	this.setState({
+
+	 		data: [
+
 						  {
 						    id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
 						    firstName: "Freda",
@@ -43,13 +68,49 @@ import "../styles/Badges.css"
 						    twitter: "heysoypaez",
 						    avatarUrl: "https://www.gravatar.com/avatar/e74e87d40e55b9ff4491c78892e55cb7?d=identicon"
 						  },
-						]
- 	}
+	 		],
+	 		loading: false
+	 	})
+	 } , 
+	 3000
+	);
+
+}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log(5, "componentDidUpdate(prevProps, prevState)")
+
+		console.log(
+		  {prevProps: prevProps}, 
+		  {prevState: prevState}
+		)
+
+				console.log(
+		  {Props: this.props}, 
+		  {State: this.state}
+		)
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.timeoutId);
+		//Limpiamos memoria
+		console.log(6, "componentWillUnmount()" )
+
+		
+	}
 
 	render() {
+
+		console.log(2,4, "render")
 		
 		return (
 				<section className="Badges">
+
+					{
+						(this.state.loading) &&
+						<h1>Patience my friend...</h1>
+					}
+					
 
 					<section className="Badges__hero">
 						<section className="Badges__container">
