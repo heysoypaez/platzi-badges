74898c6ed1d8ebf4c453bfb1505f9eef25cb48c7
Daniel Páez <danielpaezsw66@gmail.com>
2019-08-23 15:32:24 -0400

    Creating manually webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js"
-import "./global.css"
+import App from "./App.js";
+import "./global.css";
 
 const app = document.getElementById('app');
 
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
+
+module.exports = {
+
+	entry: {
+		app: path.resolve( __dirname, "src/index.js" ),
+	},
+	mode:"production",
+
+	output: {
+		path: path.resolve( __dirname , "dist/" ),
+		filename: "js/[name].[hash].js",
+		publicPath: "http://localhost:3001/",
+		chunkFilename: "js/[id].[chunkhash].js "
+	},
+	module: {
+
+		rules: [
+
+			{
+				test: /\.css$/,
+				use: [ 
+
+					{
+						loader: MiniCssExtractPlugin.loader 
+					},
+					
+					"css-loader"
+				]
+			},
+				/*Css Preprocessor END
+		------------------------*/
+			{
+				test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+				use: {
+					loader: "url-loader",
+					options: {
+						limit: 50000, //limit bites of size
+						name: "[hash].[ext]",
+						outputPath: "assets"
+					}				
+				}	
+			},
+			{
+				test: /\.js$/,
+				use: "babel-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+
+
+		new MiniCssExtractPlugin({
+			filename: "css/[name].[hash].css",
+			chunkFilename: "css/[id].[hash].css"
+		}),	
+
+		new HtmlWebpackPlugin({
+			minify: {
+				collapseWhitespace: true
+			},
+			template:  path.resolve( __dirname, "public/index.html" )	
+		}),
+		new webpack.DllReferencePlugin({
+			manifest: require("./modules-manifest.json")
+		}),
+
+		 new AddAssetHtmlPlugin({ 
+		 	filepath: path.resolve( __dirname, './dist/js/*.dll.js'),
+		 	outputPath: "js",
+		 	publicPath: "http://localhost:3001/js" 
+		 })
+	]
+}
+
+//	npx  webpack --entry ./index.js --output ./bundle.js
+// webpack por defecto cuando escribo nppc webpack ejecuta este archivo
